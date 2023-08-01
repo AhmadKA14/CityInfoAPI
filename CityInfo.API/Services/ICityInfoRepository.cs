9d0627788ca08e181dfc540f46e04b3f544d4b5e
AhmadKA14 <ahmadkhalil822003@gmail.com>
2023-07-31 22:37:28 -0400

    Add project files.

---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(string? name, string? searchQuery, int pageNumber, int pageSize);
+        Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestAsync(int cityId);
+        Task<PointOfInterest?> GetPointOfInterestAsync(int cityId, int pointOfInterestId);
+        Task<bool> CityExistsAsync(int cityId);
+        Task AddPointOfInterestForCityAsync(int cityId, PointOfInterest pointOfInterest);
+        void DeletePointOfInterest(PointOfInterest pointOfInterest);
+        Task<bool> SaveChangesAsync();
+    }
+}
